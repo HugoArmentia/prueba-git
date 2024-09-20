3738d57224ebc8e0e174ad1374190c9fb6774ee3
Hugo Armentia <armentia.hugo@opendeusto.es>
2024-09-20 08:37:37 +0200

    .

--- PruebaGit/src/mundo.java
+++ PruebaGit/src/mundo.java
@@ -3,6 +3,6 @@ public class mundo {
 	
 	public static void main(String[] args) {
 		System.out.println("Adios mundo!!!!");
-		System.out.printl("Esto es otro mensaje");
+		System.out.println("Esadfaefdasdf");
 	}
 }
